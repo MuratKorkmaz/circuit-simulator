35925a7937b8fcc026d72be3918c2b014d727114
dmitry <dmitry@localhost>
2012-02-21 21:27:54 +0600

    Fixed correct menu

--- JavaToSharp/FormMain.Designer.cs
+++ JavaToSharp/FormMain.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace JavaToSharp
+﻿using System.Windows.Forms;
+
+namespace JavaToSharp
 {
     partial class FormMain
     {
@@ -53,7 +55,13 @@
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(700, 24);
             this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Text = "enuStrip";
+            this.menuStrip1.Items.AddRange(new ToolStripItem[]
+                                               {
+                                                   файлToolStripMenuItem,
+                                                   осциллографToolStripMenuItem,
+                                                   схемыToolStripMenuItem
+                                               });
             // 
             // файлToolStripMenuItem
             // 
